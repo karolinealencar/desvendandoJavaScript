1e548fab2a9701dd21c57c14c967dcc6c8149428
ravenNNN <baltazarparra@outlook.com>
2015-10-18 11:06:26 -0200

    Aula Number Concluida

---
+++ aulas/introducao.js
@@ -0,0 +1,23 @@
+// Variáveis
+
+// Nome de variável deve começar por uma letraMinúscula, $ ou _
+// $variável - Reservado para FrameWorks. (Jquery, Angular, Etcs.)
+// _variável - Reservado para variáveis privadas. (Dentro de funções, para sinalizar membros exclusivos)
+
+// Podemos usar números, mas não é uma boa prática.
+// Seguindo as boas práticas, declaramos variáveis usando *camelCase.
+// *(primeira letra minúscula e quando houver mais de uma palavra, ela vai começar com letra maiúscula, ex: minhaVariavel, novaVariavel, umaVariavelComNomeLongo)
+
+// Exemplos
+
+var name = "desvendandoJavascript";
+// Variável comum, usando camelCase
+
+var top3 = [1, 2, 3];
+// Variável usanndo números
+
+var $lib = {titulo: "Desvendando", edicao: "1"};
+// Variável referênciando um FrameWork/Biblioteca
+
+var _exclusiva = "Segredo";
+// Variável referênciada a uma função
