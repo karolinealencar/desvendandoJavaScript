0ff82abc1c23e3ba4bd51a99dbd9cec61b0dd864
Karoline Alencar <karolinedealencaralves@outlook.com.br>
2015-10-19 17:55:07 -0200

    Create numeros.js
    ♥

---
+++ aulas/numeros.js
@@ -0,0 +1,23 @@
+//Números
+
+//Em JS, há apenas um tipo de número, ou seja, não há a preocupação em especificar se é inteiro, por exemplo.
+//Podem ser escritos com ou sem decimais.
+//Ao digitar Math. irá aparecer diversos comandos que podem ser utilizados.
+
+//Exemplos
+
+var nota = 5;
+
+nota.toString();
+//Converte em string.
+
+nota.valueOf();
+//Retorna o valor da var.
+
+//MATH exemplos
+
+Math.random();
+//Irá retornar um número aleatório menor que 1.
+
+Math.cos(2);
+//Retorna o coseno de 2.
